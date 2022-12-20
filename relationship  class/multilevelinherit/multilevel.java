ad15fafa8fe672c9b229f5dd77c77c6bf4378011
abhinav soni <abhinavsoni316@gmail.com>
2022-12-20 16:02:31 +0530

    student strength section included
     here,  i include
    another section that tell us about student strength in school
    using multilevel inheritance

--- relationship  class/multilevelinherit/multilevel.java
+++ relationship  class/multilevelinherit/multilevel.java
@@ -7,11 +7,11 @@ class school
    
     int school_name()
     { 
-        System.out.println("welcome to alpha male school");
+        System.out.println("welcome to alpha male school ");
         return 0;
     }
      int total_grade()
-    {   System.out.println("enter number of grade");
+    {   System.out.print("enter number of grade :");
         Scanner ng = new Scanner(System.in);
         g = ng.nextInt();
         System.out.println("total grade in our school :" +g);
@@ -23,13 +23,14 @@ class school
 
 class grade extends school
 {
-   
+    int student_per_section,st;   
     int s;
     int total_section()
     {
         int ts;
         ts = g*s;
          System.out.println("total section is :"+ ts);
+         st =ts;
         return 0;
 
     }
@@ -47,9 +48,20 @@ class grade extends school
 
 class display extends grade 
 {
+    int total_student;
    display()
    {
     System.out.println("information regarding school will be displayed here :");
+   }
+
+   int student_strength()
+   {
+    Scanner stren = new Scanner(System.in);
+    System.out.print("enter number of student per section : ");
+    student_per_section = stren.nextInt();
+    total_student = st *student_per_section;
+    System.out.print("total number of student in school are : " + total_student);  
+    return 0;
    }
 }
 
@@ -65,6 +77,6 @@ public class multilevel
         ds.total_grade();
         ds.section();
         ds.total_section();
-       
+        ds.student_strength();       
     }
 }
