da3be25b46736538b704bd2181e6d42b96a6fb96
abhinav soni <abhinavsoni316@gmail.com>
2022-12-29 13:20:28 +0530

    relationship class folder organised

---
+++ programs/multable.java
@@ -0,0 +1,16 @@
+
+        
+public class multable 
+{
+    public static void main(String[] args)
+     {
+      for(int i = 1;i<=10;i++)
+      {
+        
+            System.out.printf("%10s \n \t \t ",i*2);
+
+      }  
+    }
+
+    
+}
